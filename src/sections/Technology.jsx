594685c3c5268150f9a4f7f7010f2d7d341c8adc
Yauheniya <yauheniya.ai@gmail.com>
2025-04-26 12:07:21 +0200

    feat: add title and icon to the Technology section

--- src/sections/Technology.jsx
+++ src/sections/Technology.jsx
@@ -1,4 +1,5 @@
 import { logoIconsList } from "../constants";
+import TitleHeader from "../components/TitleHeader";
 import GlowCard from "../components/GlowCard"
 
 const LogoIcon = ({ icon, index }) => {
@@ -13,10 +14,26 @@ const LogoIcon = ({ icon, index }) => {
 
 const Technology = () => (
   <div className="md:my-20 my-10 relative">
-    <div className="gradient-edge" />
+    <TitleHeader
+      sub={
+        <span className="flex items-center gap-2">
+          <img 
+            src="/public/images/engineering.svg" 
+            width="24" 
+            height="24" 
+            alt="Robot arm"
+            className="text-white transform scale-x-[-1]"
+            style={{ transform: 'scaleX(+1)' }}
+          />
+          Tech Stack
+        </span>
+      }
+      title="Technologies to Build and Deploy AI Agents"
+    />
+
     <div className="gradient-edge" />
     <div className="marquee h-52">
-      <div className="marquee-box md:gap-12 gap-5">
+      <div className="marquee-box md:gap-12 gap-5 mt-16">
         {/* First copy of icons */}
         {logoIconsList.map((icon, index) => (
           <LogoIcon key={`first-${index}`} icon={icon} />
