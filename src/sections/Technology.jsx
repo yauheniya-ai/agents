05deddfe80748139a55eafedb1bc31e82dddf7e2
Yauheniya <yauheniya.ai@gmail.com>
2025-04-26 11:43:40 +0200

    feat: integrate GlowCard effect with technology logos for interactive glow

--- src/sections/Technology.jsx
+++ src/sections/Technology.jsx
@@ -1,10 +1,13 @@
 import { logoIconsList } from "../constants";
+import GlowCard from "../components/GlowCard"
 
-const LogoIcon = ({ icon }) => {
+const LogoIcon = ({ icon, index }) => {
   return (
-    <div className="flex-none flex-center marquee-item">
-      <img src={icon.imgPath} alt={icon.name} />
-    </div>
+    <GlowCard card={{}} index={index}>
+      <div className="flex-none flex-center marquee-item">
+        <img src={icon.imgPath} alt={icon.name} />
+      </div>
+    </GlowCard>
   );
 };
 
@@ -12,7 +15,6 @@ const Technology = () => (
   <div className="md:my-20 my-10 relative">
     <div className="gradient-edge" />
     <div className="gradient-edge" />
-
     <div className="marquee h-52">
       <div className="marquee-box md:gap-12 gap-5">
         {/* First copy of icons */}
