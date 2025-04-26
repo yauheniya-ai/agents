0af9f3041d327a302b06d02b12afaf11181c9c4f
Yauheniya <yauheniya.ai@gmail.com>
2025-04-26 11:36:26 +0200

    refactor: rename ShowcaseSection.jsx to Showcase.jsx and update references in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import Footer from "./sections/Footer";
-import Contact from "./sections/Contact";
-import Hero from "./sections/Hero";
-import ShowcaseSection from "./sections/ShowcaseSection";
-import Technology from "./sections/Technology";
 import Navbar from "./components/NavBar";
+import Hero from "./sections/Hero";
+import Showcase from "./sections/Showcase";
+import Technology from "./sections/Technology";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => (
   <>
     <Navbar />
     <Hero />
-    <ShowcaseSection />
+    <Showcase />
     <Technology />
     <Contact />
     <Footer />
